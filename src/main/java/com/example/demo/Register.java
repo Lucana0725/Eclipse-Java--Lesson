e1e7827b86f3defb467c91fd6256bb549c45a21e
Lucana <k.ark.scarlet72@gmail.com>
2023-06-01 18:55:30 +0900

    [Finish]register.htmlを作成、送信ボタンが押下されたときにregisterメソッドが動作し、RegisterBeanクラスでメンバー変数に代入された値がMAVにて保持されregister.htmlへ送信し、表示されている。

--- src/main/java/com/example/demo/Register.java
+++ src/main/java/com/example/demo/Register.java
@@ -16,8 +16,8 @@ public class Register {
 	@RequestMapping("/register")
 	public ModelAndView register  // input.htmlで「送信」が押されたら動くメソッド
 		(@ModelAttribute RegisterBean rb, ModelAndView mav) {
-			// 引数として ①RegisterBeanクラスのrbというオブジェクトを受け取る(@ModelAttributeはinput.htmlで送信された値をRegisterBeanのメンバー変数に自動的に割り当ててくれる)
-			// ②ModelAndViewクラスのmavというオブジェクト。モデルとビューの情報を保持するためのオブジェクト。
+				// 引数として ①RegisterBeanクラスのrbというオブジェクトを受け取る(@ModelAttributeはinput.htmlで送信された値をRegisterBeanのメンバー変数に自動的に割り当ててくれる)
+				// ②ModelAndViewクラスのmavというオブジェクト。モデルとビューの情報を保持するためのオブジェクト。
 		
 			mav.addObject("rb", rb);  // モデルの情報をもたせる"addObject"メソッド
 			mav.setViewName("register.html");  // ビューの情報をもたせる"setViewName"メソッド
